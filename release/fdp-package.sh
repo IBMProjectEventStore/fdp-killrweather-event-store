c9b94ba6d2df256c8b5282f161a2b9e061c20226
Dean Wampler <dean@concurrentthought.com>
2018-06-14 11:16:58 -0500

    New release process and doc tweaks

--- release/fdp-package.sh
+++ release/fdp-package.sh
@@ -9,7 +9,7 @@ ROOT_DIR="${DIR}/.."
 
 function usage {
   cat<< EOF
-  fdp-killrweather:
+  fdp-killrweather-event-store:
   This script currently builds the software, including docker images (but doesn't push them).
   It also creates an archive of the code.
   Usage: $SCRIPT [VERSION] [-h | --help]
@@ -49,7 +49,7 @@ echo "$0: Processing templates for config files:"
 
 $ROOT_DIR/process-templates.sh $VERSION
 
-OUTPUT_FILE_ROOT=fdp-killrweather-${VERSION}
+OUTPUT_FILE_ROOT=fdp-killrweather-event-store-${VERSION}
 OUTPUT_FILE=${OUTPUT_FILE_ROOT}.zip
 
 echo "$0: Building the zip file of sources: $OUTPUT_FILE"
@@ -66,7 +66,7 @@ cd  ${ROOT_DIR}
 for f in *
 do
   case $f in
-    target|release|Jenkins*|*Lightbend*.md)
+    target|release|Jenkins*)
       # skipping
       ;;
     *)
@@ -78,9 +78,8 @@ done
 # Remove files and directories that shouldn't be in the distribution.
 # Some of these should have been filtered in the previous do loop.
 cd $staging
-rm -f ${OUTPUT_FILE_ROOT}/README-Lightbend.md
-# TODO: remove the next line after v1.2!
-rm -f ${OUTPUT_FILE_ROOT}/README-Kubernetes.md
+# TODO: Clean up this file to remove references to Cassandra, then remove the next line.
+rm -f ${OUTPUT_FILE_ROOT}/README-DEVELOPERS.md
 find ${OUTPUT_FILE_ROOT} \( -name whitesource.sbt -o -name WhitesourceLicensePlugin.scala \) -exec rm {} \;
 find ${OUTPUT_FILE_ROOT} -type d | egrep 'project/(project|target)$' | while read d; do rm -rf "$d"; done
 find ${OUTPUT_FILE_ROOT} -type d | egrep 'target$' | while read d; do rm -rf "$d"; done
@@ -90,7 +89,7 @@ zip -r ${OUTPUT_FILE} ${OUTPUT_FILE_ROOT}
 
 rm -rf ${OUTPUT_FILE_ROOT}
 
-echo "$0: Building the sample apps and docker images: $ROOT_DIR/build.sh"
+echo "$0: Building the Killrweather sample apps and docker images: $ROOT_DIR/build.sh"
 
 $ROOT_DIR/build.sh $VERSION
 
